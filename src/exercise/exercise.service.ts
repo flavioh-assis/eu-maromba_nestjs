42e1e1fa46201b99911aeeb85716bd222e25c8c3
Flavio Assis <flavioh.dev@gmail.com>
2023-01-16 11:44:33 -0300

    Implementa novo retorno no service de exercise

--- src/exercise/exercise.service.ts
+++ src/exercise/exercise.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaClientUnknownRequestError } from '@prisma/client/runtime';
+import { errorOnFind, successOnFind } from 'src/app.response';
 
 const prisma = new PrismaClient();
 
@@ -7,10 +9,13 @@ const prisma = new PrismaClient();
 export class ExerciseService {
   async findAll() {
     try {
-      return await prisma.exercise.findMany();
+      const response = await prisma.exercise.findMany();
+
+      return successOnFind(response);
     } catch (error) {
       console.log(error);
-      return [];
+
+      return errorOnFind(error as PrismaClientUnknownRequestError);
     } finally {
       await prisma.$disconnect();
     }
