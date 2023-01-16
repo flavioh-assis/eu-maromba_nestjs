88e1a4ef5e2b5cc65a5c86a73ae73309b8ddfe09
Flavio Assis <flavioh.dev@gmail.com>
2023-01-16 12:54:31 -0300

    Cria metodo post de training

--- src/training/training.controller.ts
+++ src/training/training.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Training } from '@prisma/client';
 import { TrainingService } from './training.service';
 
 @Controller('api/trainings')
 export class TrainingController {
   constructor(private readonly service: TrainingService) {}
+
+  @Post()
+  async create(@Body() training: Training) {
+    return this.service.create(training);
+  }
 
   @Get()
   async findAll() {
--- src/training/training.service.ts
+++ src/training/training.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Training } from '@prisma/client';
 import { PrismaClientUnknownRequestError } from '@prisma/client/runtime';
-import { errorOnFind, successOnFind } from 'src/app.response';
+import {
+  errorOnCreate,
+  errorOnFind,
+  successOnCreate,
+  successOnFind,
+} from 'src/app.response';
 
 const prisma = new PrismaClient();
 
 @Injectable()
 export class TrainingService {
+  async create(training: Training) {
+    try {
+      const response = await prisma.training.create({
+        data: training,
+      });
+
+      return successOnCreate([response]);
+    } catch (error) {
+      console.log(error);
+
+      return errorOnCreate(error as PrismaClientUnknownRequestError);
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+
   async findAll() {
     try {
       const response = await prisma.training.findMany();
