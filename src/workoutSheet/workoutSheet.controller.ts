d93b180a29617032eb341f3587e045ce797a190a
Flavio Assis <flavioh.dev@gmail.com>
2023-01-17 11:28:20 -0300

    Cria endpoint de update workout sheet

--- src/workoutSheet/workoutSheet.controller.ts
+++ src/workoutSheet/workoutSheet.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post, Put } from '@nestjs/common';
+import { errorOnValidate } from 'src/app.response';
+import { validateId } from 'src/app.validator';
 import { WorkoutSheetService } from './workoutSheet.service';
 
 @Controller('api/workout-sheets')
@@ -14,5 +16,12 @@ export class WorkoutSheetController {
   @Get()
   async findAll() {
     return this.service.findAll();
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body('name') name: string) {
+    if (!validateId(id)) return errorOnValidate(`Id {${id}} is not valid.`);
+
+    return this.service.update(id, name);
   }
 }
