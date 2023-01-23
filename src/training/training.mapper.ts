31f32bef29f17e1227bb5bf4d902b29321b80b13
Flavio Assis <flavioh.dev@gmail.com>
2023-01-23 17:22:25 -0300

    Ajustes no mapper

--- src/training/training.mapper.ts
+++ src/training/training.mapper.ts
@@ -1,7 +1,17 @@
 import { Training } from '@prisma/client';
-import { CreateTrainingRequest } from './type/training.request';
+import { CreateTrainingRequest, EditTrainingRequest } from './type/training.request';
 
-export function mapTraining(request: CreateTrainingRequest) {
+export function mapTrainingEdit(request: EditTrainingRequest) {
+  return {
+    workoutSheetId: request.workoutSheet.id,
+    sets: request.sets,
+    reps: request.reps,
+    restTime: request.restTime,
+    obs: request.obs,
+  } as Training;
+}
+
+export function mapTrainingCreate(request: CreateTrainingRequest) {
   return {
     exerciseId: request.exercise.id,
     workoutSheetId: request.workoutSheet.id,
