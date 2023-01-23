f8f5f497b852efcd3329935955aebfb310127ff5
Flavio Assis <flavioh.dev@gmail.com>
2023-01-23 13:17:46 -0300

    Cria tipos de resposta

---
+++ src/type.ts
@@ -0,0 +1,43 @@
+export type RequestResponse = {
+  success: boolean;
+  statusCode: number;
+  statusText: string;
+  data:
+    | ExerciseResponse[]
+    | MuscleGroupResponse[]
+    | TrainingResponse[]
+    | WorkoutSheetResponse[];
+};
+
+export type ExerciseResponse = {
+  id: number;
+  name: string;
+  muscleGroup: { id: number };
+};
+
+export type MuscleGroupResponse = {
+  id: number;
+  name: string;
+};
+
+export type TrainingResponse = {
+  id: number;
+  exercise: {
+    id: number;
+    name: string;
+  };
+  workoutSheet: {
+    id: number;
+    name: string;
+  };
+  sets: string;
+  reps: string;
+  restTime: string;
+  obs: string;
+};
+
+export type WorkoutSheetResponse = {
+  id: number;
+  name: string;
+  trainings: TrainingResponse[];
+};
