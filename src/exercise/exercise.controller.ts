15ac509eb65fc9c94879dff4b08ed248562916ca
Flavio Assis <flavioh.dev@gmail.com>
2023-01-26 10:00:04 -0300

    Ajusta endpoint para buscar por muscle group

--- src/exercise/exercise.controller.ts
+++ src/exercise/exercise.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
-import { PrismaClientUnknownRequestError } from '@prisma/client/runtime';
-import { successOnFindMany, errorOnFind } from 'src/response';
+import { Controller, Get, Query } from '@nestjs/common';
+import { successOnFindMany, errorOnFind, errorOnValidate } from 'src/response';
+import { validateId } from 'src/validator';
 import { ExerciseService } from './exercise.service';
 
 @Controller('api/exercises')
@@ -8,15 +8,25 @@ export class ExerciseController {
   constructor(private readonly service: ExerciseService) {}
 
   @Get()
-  async findAll() {
+  async findAll(@Query('muscleGroupId') query: string) {
     try {
-      const response = await this.service.findAll();
+      if (!query) {
+        const exercises = await this.service.findAll();
 
-      return successOnFindMany(response);
+        return successOnFindMany(exercises);
+      }
+
+      if (!validateId(query)) {
+        return errorOnValidate(`Param muscleGroupId {${query}} is not valid.`);
+      }
+
+      const exercises = await this.service.findForMuscleGroup(+query);
+
+      return successOnFindMany(exercises);
     } catch (error) {
       console.log(error);
 
-      return errorOnFind(error as PrismaClientUnknownRequestError);
+      return errorOnFind(error);
     }
   }
 }
