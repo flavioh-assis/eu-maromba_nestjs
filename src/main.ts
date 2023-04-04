3217ad24a9d32960f7219e915c6aa09590cd9dec
Flavio Assis <flavioh.dev@gmail.com>
2023-04-04 19:40:20 -0300

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +9,20 @@ async function bootstrap() {
     new FastifyAdapter()
   );
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Eu Maromba API')
+    .setDescription('API documentation para o projeto Eu Maromba')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      tryItOutEnabled: true,
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
