2bdab345f09a40906dc5617a7a13d922aff26c14
Flavio Assis <flavioh.dev@gmail.com>
2023-04-05 15:36:31 -0300

    Add ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    })
   );
   app.setGlobalPrefix('api');
 
