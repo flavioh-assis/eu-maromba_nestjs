012a40e0c7c0bae051848c228db95abde9dd6625
Flavio Assis <flavioh.dev@gmail.com>
2023-04-05 20:15:07 -0300

    Ajustes funçoes

--- src/workoutSheet/workoutSheet.service.ts
+++ src/workoutSheet/workoutSheet.service.ts
@@ -1,24 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { db } from 'src/db.connection';
 import { selectTrainingResponse } from 'src/training/training.constant';
-import { CreateWorkoutSheetDto, EditWorkoutSheetDto } from './type/workoutSheet.dto';
+import { EditWorkoutSheetDto } from './type/workoutSheet.dto';
 import { WorkoutSheetResponse } from './type/workoutSheet.response';
+import { WorkoutSheet } from '@prisma/client';
 
 @Injectable()
 export class WorkoutSheetService {
-  async create(workoutSheet: CreateWorkoutSheetDto) {
+  async create(workoutSheet: WorkoutSheet) {
     const dbResult = await db.workoutSheet.create({
       data: workoutSheet,
-      select: {
-        id: true,
-        name: true,
-        trainings: {
-          orderBy: {
-            position: 'asc',
-          },
-          select: selectTrainingResponse,
-        },
-      },
     });
 
     return dbResult as WorkoutSheetResponse;
@@ -26,39 +17,28 @@ export class WorkoutSheetService {
 
   async findAll() {
     const dbResult = await db.workoutSheet.findMany({
-      select: {
-        id: true,
-        name: true,
-        trainings: {
-          orderBy: {
-            position: 'asc',
-          },
-          select: selectTrainingResponse,
-        },
+      orderBy: {
+        position: 'asc',
       },
     });
 
     return dbResult as WorkoutSheetResponse[];
   }
 
-  async findOne(id: number) {
-    const dbResult = await db.workoutSheet.findUnique({
-      where: {
-        id,
-      },
+  async findLastPosition() {
+    const dbResult = await db.workoutSheet.findMany({
       select: {
-        id: true,
-        name: true,
-        trainings: {
-          orderBy: {
-            position: 'asc',
-          },
-          select: selectTrainingResponse,
-        },
+        position: true,
       },
+      orderBy: [
+        {
+          position: 'desc',
+        },
+      ],
+      take: 1,
     });
 
-    return dbResult as WorkoutSheetResponse;
+    return dbResult.length ? dbResult[0].position : 0;
   }
 
   async update(id: number, workoutSheet: EditWorkoutSheetDto) {
