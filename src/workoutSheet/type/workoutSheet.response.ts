fca6b55578f38efeceba41a2cbc8f6860bd92472
Flavio Assis <flavioh.dev@gmail.com>
2023-04-06 18:55:18 -0300

    Remove trainings do tipo resposta

--- src/workoutSheet/type/workoutSheet.response.ts
+++ src/workoutSheet/type/workoutSheet.response.ts
@@ -1,8 +1,5 @@
-import { TrainingResponse } from 'src/training/type/training.response';
-
 export type WorkoutSheetResponse = {
   id: number;
   name: string;
   position: number;
-  trainings: TrainingResponse[];
 };
