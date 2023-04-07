8989e1c661eafd49dda394e3d7c8cd5cf68b99ba
Flavio Assis <flavioh.dev@gmail.com>
2023-04-07 12:36:57 -0300

    Remove endpointfindAll, ordena por nome retorno de finAll por MG

--- src/exercise/exercise.controller.ts
+++ src/exercise/exercise.controller.ts
@@ -1,32 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { successOnFindMany, errorOnFind, errorOnValidate } from 'src/response';
-import { validateId } from 'src/validator';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ExerciseService } from './exercise.service';
 
-@Controller('exercises')
+@Controller()
 export class ExerciseController {
   constructor(private readonly service: ExerciseService) {}
 
-  @Get()
-  async findAll(@Query('muscleGroupId') query: string) {
-    try {
-      if (!query) {
-        const exercises = await this.service.findAll();
-
-        return successOnFindMany(exercises);
-      }
-
-      if (!validateId(query)) {
-        return errorOnValidate(`Param muscleGroupId {${query}} is not valid.`);
-      }
-
-      const exercises = await this.service.findForMuscleGroup(+query);
-
-      return successOnFindMany(exercises);
-    } catch (error) {
-      console.log(error);
-
-      return errorOnFind(error);
-    }
+  @Get('muscle-groups/:id/exercises')
+  async findAllForMuscleGroup(@Param('id') muscleGroupId: number) {
+    return await this.service.findAllForMuscleGroup(muscleGroupId);
   }
 }
--- src/exercise/exercise.service.ts
+++ src/exercise/exercise.service.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { db } from 'src/db.connection';
-import { selectExerciseResponse } from './exercise.constant';
-import { ExerciseResponse } from './type/exercise.response';
 
 @Injectable()
 export class ExerciseService {
-  async findAll() {
-    const dbResult = await db.exercise.findMany({
-      select: selectExerciseResponse,
-    });
-
-    return dbResult as ExerciseResponse[];
-  }
-
-  async findForMuscleGroup(muscleGroupId: number) {
-    const dbResult = await db.exercise.findMany({
+  async findAllForMuscleGroup(muscleGroupId: number) {
+    return await db.exercise.findMany({
       where: {
         muscleGroupId,
       },
-      select: selectExerciseResponse,
+      select: {
+        id: true,
+        name: true,
+      },
+      orderBy: [
+        {
+          name: 'asc',
+        },
+      ],
     });
-
-    return dbResult as ExerciseResponse[];
   }
 }
