8bf2df3d7935706757aa230c7731e5997d377e62
Flavio Assis <flavioh.dev@gmail.com>
2023-04-07 12:46:41 -0300

    add tag

--- src/exercise/exercise.controller.ts
+++ src/exercise/exercise.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ExerciseService } from './exercise.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Exercise')
 @Controller()
 export class ExerciseController {
   constructor(private readonly service: ExerciseService) {}
