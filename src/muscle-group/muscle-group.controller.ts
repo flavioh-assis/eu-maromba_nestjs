8272c3e94da0ebc1d161688296a4455a4e42b1cf
Flavio Assis <flavioh.dev@gmail.com>
2023-04-07 11:45:26 -0300

    Add tag

--- src/muscle-group/muscle-group.controller.ts
+++ src/muscle-group/muscle-group.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { MuscleGroupService } from './muscle-group.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Muscle Group')
 @Controller('muscle-groups')
 export class MuscleGroupController {
   constructor(private readonly service: MuscleGroupService) {}
