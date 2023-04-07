5d04500ae75a88945c6fccf8b363cf71f8f61fe7
Flavio Assis <flavioh.dev@gmail.com>
2023-04-07 12:48:19 -0300

    Remove arquivos desnecessarios

--- src/type.ts
+++
@@ -1,8 +0,0 @@
-import { TrainingResponse } from './training/type/training.response';
-
-export type RequestResponse = {
-  success: boolean;
-  statusCode: number;
-  statusText: string;
-  data: TrainingResponse[];
-};
--- src/validator.ts
+++
@@ -1,3 +0,0 @@
-export function validateId(id: string) {
-  return Number(id) > 0;
-}
