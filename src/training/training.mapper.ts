1c868ce1e7ee97d0bd23d2cdcedbc3b53788148f
Flavio Assis <flavioh.dev@gmail.com>
2023-04-11 19:20:49 -0300

    Ajusta mapTrainingUpdate

--- src/training/training.mapper.ts
+++ src/training/training.mapper.ts
@@ -1,14 +1,23 @@
 import { Training } from '@prisma/client';
-import { CreateTrainingRequest, EditTrainingRequest } from './type/training.request';
+import { CreateTrainingRequest, UpdateTrainingDto } from './type/training.request';
 
-export function mapTrainingEdit(request: EditTrainingRequest) {
-  return {
-    workoutSheetId: request.workoutSheet.id,
-    sets: request.sets,
-    reps: request.reps,
-    restTime: request.restTime,
-    obs: request.obs,
+export function mapTrainingUpdate(dto: UpdateTrainingDto) {
+  const training = {
+    sets: dto?.sets,
+    reps: dto?.reps,
+    restTime: dto?.restTime,
+    obs: dto?.obs,
+    workoutSheetId: dto?.workoutSheet?.id,
+    exerciseId: dto?.exercise?.id,
   } as Training;
+
+  (Object.keys(training) as (keyof Training)[]).forEach(key => {
+    if (training[key] === undefined) {
+      delete training[key];
+    }
+  });
+
+  return training;
 }
 
 export function mapTrainingCreate(
