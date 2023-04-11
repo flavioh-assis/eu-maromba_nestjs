616f6f95b504312f10edbb2e7bcb3bcb02e6221e
Flavio Assis <flavioh.dev@gmail.com>
2023-04-11 19:17:17 -0300

    Cria WorkoutSheetDto

--- src/workout-sheet/type/workout-sheet.dto.ts
+++ src/workout-sheet/type/workout-sheet.dto.ts
@@ -1,3 +1,4 @@
+import { PickType } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsNumber, Min } from 'class-validator';
 
@@ -12,11 +13,11 @@ export class EditWorkoutSheetDto extends CreateWorkoutSheetDto {}
 export class ReorderWorkoutSheetDto {
   @IsNumber()
   @Min(1)
-  @Transform(({ value }) => Number(value))
   id!: number;
 
   @IsNumber()
   @Min(1)
-  @Transform(({ value }) => Number(value))
   position!: number;
 }
+
+export class WorkoutSheetDto extends PickType(ReorderWorkoutSheetDto, ['id'] as const) {}
