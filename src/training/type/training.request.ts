d6c3e8542050e5161272001825bfbda883e27502
Flavio Assis <flavioh.dev@gmail.com>
2023-04-11 19:19:43 -0300

    Ajusta UpdateTrainingDto e ReorderTrainingDto

--- src/training/type/training.request.ts
+++ src/training/type/training.request.ts
@@ -1,3 +1,49 @@
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, IsString, Min, ValidateNested } from 'class-validator';
+import { ExerciseDto } from 'src/exercise/type/exercise.dto';
+import { WorkoutSheetDto } from 'src/workout-sheet/type/workout-sheet.dto';
+
+export class UpdateTrainingDto {
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  sets?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  reps?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  restTime?: number;
+
+  @IsOptional()
+  @IsString()
+  obs?: string;
+
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => ExerciseDto)
+  exercise?: ExerciseDto;
+
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => WorkoutSheetDto)
+  workoutSheet?: WorkoutSheetDto;
+}
+
+export class ReorderTrainingDto {
+  @IsNumber()
+  @Min(1)
+  id!: number;
+
+  @IsNumber()
+  @Min(1)
+  position!: number;
+}
+
 export type CreateTrainingRequest = {
   exercise: {
     id: number;
@@ -6,19 +52,4 @@ export type CreateTrainingRequest = {
   reps: number;
   restTime: number;
   obs: string;
-};
-
-export type EditTrainingRequest = {
-  workoutSheet: {
-    id: number;
-  };
-  sets: number;
-  reps: number;
-  restTime: number;
-  obs: string;
-};
-
-export type ReorderTrainingRequest = {
-  id: number;
-  position: number;
 };
