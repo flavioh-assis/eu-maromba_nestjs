b898946d4e4cf90b7d6169069dfa85d174cba621
Flavio Assis <flavioh.dev@gmail.com>
2023-04-12 19:25:35 -0300

    Add example

--- src/exercise/type/exercise.dto.ts
+++ src/exercise/type/exercise.dto.ts
@@ -1,7 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, Min } from 'class-validator';
 
 export class ExerciseDto {
   @IsNumber()
   @Min(1)
+  @ApiProperty({
+    example: 1,
+  })
   id!: number;
 }
