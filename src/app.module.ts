a1828c63722e98c2bbc30ae01bbb9856d921a6e0
Flavio Assis <flavioh.dev@gmail.com>
2023-04-12 16:46:56 -0300

    Registra novas rotas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
+import { routes } from './routes';
 import { ExerciseModule } from './exercise/exercise.module';
 import { MuscleGroupModule } from './muscle-group/muscle-group.module';
 import { TrainingModule } from './training/training.module';
 import { WorkoutSheetModule } from './workout-sheet/workout-sheet.module';
 
 @Module({
-  imports: [MuscleGroupModule, ExerciseModule, TrainingModule, WorkoutSheetModule],
+  imports: [
+    RouterModule.register(routes),
+    MuscleGroupModule,
+    ExerciseModule,
+    TrainingModule,
+    WorkoutSheetModule,
+  ],
 })
 export class AppModule {}
